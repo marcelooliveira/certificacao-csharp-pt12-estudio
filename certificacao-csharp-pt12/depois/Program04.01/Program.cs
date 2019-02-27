d79cfa4ec2d573014d43be4dd8513e797c20bda9
marcelooliveira <mclricardo@gmail.com>
2019-02-27 18:23:36 -0300

    .

--- certificacao-csharp-pt12/depois/Program04.01/Program.cs
+++ certificacao-csharp-pt12/depois/Program04.01/Program.cs
@@ -5,11 +5,10 @@ namespace Program04
 {
     class Program
     {   
-        static void Main1(string[] args)
+        static void Main(string[] args)
         {
             //Tarefa 1: separar nomes das linguagens por vírgulas:
             var entrada1 = "CSharp Java Python Ruby Swift Scala ObjectiveC";
-
 
             //Tarefa 2: separar nomes das linguagens por vírgulas:
             var entrada2 = "CSharp     Java   Python  Ruby Swift Scala ObjectiveC";
@@ -23,7 +22,17 @@ namespace Program04
             //- Campo 4: Duração do filme em minutos
             //- Id, ano e duração são campos numéricos
             //- Título do filme são letras ou espaços
-            var entrada3 = "2:O Exterminador do Futuro:1984:107";
+            var entrada3 = "123:O Exterminador do Futuro:1984:107";
+
+            bool registroValido = true;
+            if (registroValido)
+            {
+                Console.WriteLine("Registro de filme VÁLIDO");
+            }
+            else
+            {
+                Console.WriteLine("Registro de filme INVÁLIDO");
+            }
 
             Console.ReadLine();
         }
