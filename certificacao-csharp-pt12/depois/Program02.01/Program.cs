f52f0da8c71c575a97a50404ed661ec0478d2611
marcelooliveira <mclricardo@gmail.com>
2019-02-27 12:45:42 -0300

    .

--- certificacao-csharp-pt12/depois/Program02.01/Program.cs
+++ certificacao-csharp-pt12/depois/Program02.01/Program.cs
@@ -22,22 +22,22 @@ namespace Program02._01
             //prioridade. Assim, a coleção adequada é uma** fila**, ou 
             //`Queue<T>`, em inglês.
 
-            var veiculo1 = "van";
-            var veiculo2 = "kombi";
-            var veiculo3 = "guincho";
-            var veiculo4 = "pickup";
+            //var veiculo1 = "van";
+            //var veiculo2 = "kombi";
+            //var veiculo3 = "guincho";
+            //var veiculo4 = "pickup";
 
-            Queue<string> pedagio = new Queue<string>();
-            pedagio.Enqueue(veiculo1);
-            pedagio.Enqueue(veiculo2);
-            pedagio.Enqueue(veiculo3);
-            pedagio.Enqueue(veiculo4);
+            //TIPO_COLECAO<string> pedagio = new TIPO_COLECAO<string>();
+            //pedagio.ADICIONAR(veiculo1);
+            //pedagio.ADICIONAR(veiculo2);
+            //pedagio.ADICIONAR(veiculo3);
+            //pedagio.ADICIONAR(veiculo4);
 
-            Console.WriteLine(pedagio.Dequeue());
-            Console.WriteLine(pedagio.Dequeue());
-            Console.WriteLine(pedagio.Dequeue());
-            Console.WriteLine(pedagio.Dequeue());
-            Console.WriteLine();
+            //Console.WriteLine(pedagio.REMOVER());
+            //Console.WriteLine(pedagio.REMOVER());
+            //Console.WriteLine(pedagio.REMOVER());
+            //Console.WriteLine(pedagio.REMOVER());
+            //Console.WriteLine();
 
             //Trata - se de uma coleção genérica que receberá os elementos 
             //adicionados na fila pelo método `Enqueue()`. Os mesmos serão 
@@ -51,79 +51,79 @@ namespace Program02._01
             //são adicionados usando - se `Push()`, e removidos com o método 
             //`Pop()`.
 
-            string site0 = "<<vazio>>";
-            string site1 = "google.com";
-            string site2 = "caelum.com.br";
-            string site3 = "alura.com.br";
+            //string site0 = "<<vazio>>";
+            //string site1 = "google.com";
+            //string site2 = "caelum.com.br";
+            //string site3 = "alura.com.br";
 
-            Stack<string> historicoNavegador = new Stack<string>();
+            //TIPO_COLECAO<string> historicoNavegador = new TIPO_COLECAO<string>();
 
-            historicoNavegador.Push(site0);
-            Console.WriteLine("Navegando para: {0}", site1);
-            historicoNavegador.Push(site1);
-            Console.WriteLine("Navegando para: {0}", site2);
-            historicoNavegador.Push(site2);
-            Console.WriteLine("Navegando para: {0}", site3);
-            historicoNavegador.Push(site3);
+            //historicoNavegador.INCLUIR(site0);
+            //Console.WriteLine("Navegando para: {0}", site1);
+            //historicoNavegador.INCLUIR(site1);
+            //Console.WriteLine("Navegando para: {0}", site2);
+            //historicoNavegador.INCLUIR(site2);
+            //Console.WriteLine("Navegando para: {0}", site3);
+            //historicoNavegador.INCLUIR(site3);
 
-            Console.WriteLine("Voltando para: {0}", historicoNavegador.Pop());
-            Console.WriteLine("Voltando para: {0}", historicoNavegador.Pop());
-            Console.WriteLine("Voltando para: {0}", historicoNavegador.Pop());
-            Console.WriteLine("Voltando para: {0}", historicoNavegador.Pop());
-            Console.WriteLine();
+            //Console.WriteLine("Voltando para: {0}", historicoNavegador.REMOVER());
+            //Console.WriteLine("Voltando para: {0}", historicoNavegador.REMOVER());
+            //Console.WriteLine("Voltando para: {0}", historicoNavegador.REMOVER());
+            //Console.WriteLine("Voltando para: {0}", historicoNavegador.REMOVER());
+            //Console.WriteLine();
 
             //Existe um tipo de coleção, a mais flexível e poderosa de todas: 
             //a `List<T>`, uma implementação do .NET Framework que permite 
             //a inserção de um elemento em qualquer posição da coleção
             //(`Insert()`), ou especificamente no fim(`Add()` e `AddRange()`).
 
-            List<string> meses = new List<string>
-            {
-                "janeiro","fevereiro","março","abril",
-                "abril","maio","junho",
-                "julho","agosto"             
-            };
+            //TIPO_COLECAO<string> meses = new TIPO_COLECAO<string>
+            //{
+            //    "janeiro","fevereiro","março","abril",
+            //    "abril","maio","junho",
+            //    "julho","agosto"             
+            //};
 
-            meses.Add("setembro");
-            meses.AddRange(new string[] { "novembro", "dezembro", "onzembro" });
+            //meses.INCLUIR("setembro");
+            //meses.INCLUIR_FAIXA(new string[] { "novembro", "dezembro", "onzembro" });
 
             //É possível também remover elementos do meio da coleção, com 
             //`Remove()` ou `RemoveRange()`, limpar a coleção(`Clear()`) ou 
             //reverter sua ordem(`Reverse()`).
 
-            meses.RemoveAt(4);
-            meses.RemoveAt(meses.Count - 1);
-            meses.Insert(9, "outubro");
+            //meses.REMOVER_NO_INDICE(4);
+            //meses.REMOVER_NO_INDICE(meses.Count - 1);
+            //meses.INCLUIR(9, "outubro");
 
-            for (int i = 0; i < meses.Count; i++)
-            {
-                Console.WriteLine("mês {0}: {1}", i + 1, meses[i]);
-            }
-            Console.WriteLine();
+            //for (int i = 0; i < meses.Count; i++)
+            //{
+            //    Console.WriteLine("mês {0}: {1}", i + 1, meses[i]);
+            //}
+            //Console.WriteLine();
 
             //Pode -se também ordená-la por 
             //um critério qualquer, com `Sort()`.
 
-            meses.Sort((m1, m2) => m1.CompareTo(m2));
+            //meses.Sort((m1, m2) => m1.CompareTo(m2));
 
-            for (int i = 0; i < meses.Count; i++)
-            {
-                Console.WriteLine("{0}", meses[i]);
-            }
-            Console.WriteLine();
+            //for (int i = 0; i < meses.Count; i++)
+            //{
+            //    Console.WriteLine("{0}", meses[i]);
+            //}
+            //Console.WriteLine();
 
             //Caso tenhamos que lidar com arquivos de baixo nível(bytes, 
             //números inteiros, por exemplo) ou tamanho fixo, quase sempre 
             //utilizamos uma **matriz * *, ou* array*, em inglês, uma coleção 
             //de tamanho fixo que facilita o uso através do índice da coleção.
 
-            int[] imagem = new int[65535];
+            //TIPO_COLECAO imagem = new TIPO_COLECAO[65535];
 
-            int x = 31;
-            int y = 14;
+            //int x = 31;
+            //int y = 14;
 
-            const int rgb = 0x9EA3A7;
-            imagem[27] = rgb;
+            //const int rgb = 0x9EA3A7;
+            //imagem.ITEM_NA_POSICAO(27) = rgb;
 
             //Tomando como exemplo um array cujas informações gráficas, no 
             //caso, uma cor, um pixel sendo lido em uma determinada posição, 
@@ -138,19 +138,19 @@ namespace Program02._01
             //no início(`AddFirst()`), no fim(`AddLast()`), antes(`AddBefore()`)
             //ou depois(`AddAfter()`) de outro elemento da mesma coleção.
 
-            LinkedList<string> dias = new LinkedList<string>();
-            var d4 = dias.AddFirst("quarta");
-            var d2 = dias.AddBefore(d4, "segunda");
-            var d3 = dias.AddAfter(d2, "terça");
-            var d6 = dias.AddAfter(d4, "sexta");
-            var d7 = dias.AddAfter(d6, "sábado");
-            var d5 = dias.AddBefore(d6, "quinta");
-            var d1 = dias.AddBefore(d2, "domingo");
-            foreach (var dia in dias)
-            {
-                Console.WriteLine(dia);
-            }
-            Console.WriteLine();
+            //TIPO_COLECAO<string> dias = new TIPO_COLECAO<string>();
+            //var d4 = dias.ADICIONAR_PRIMEIRO("quarta");
+            //var d2 = dias.ADICIONAR_ANTES_DE(d4, "segunda");
+            //var d3 = dias.ADICIONAR_DEPOIS_DE(d2, "terça");
+            //var d6 = dias.ADICIONAR_DEPOIS_DE(d4, "sexta");
+            //var d7 = dias.ADICIONAR_DEPOIS_DE(d6, "sábado");
+            //var d5 = dias.ADICIONAR_ANTES_DE(d6, "quinta");
+            //var d1 = dias.ADICIONAR_ANTES_DE(d2, "domingo");
+            //foreach (var dia in dias)
+            //{
+            //    Console.WriteLine(dia);
+            //}
+            //Console.WriteLine();
 
             //O que caracteriza uma lista ligada é que **cada elemento é 
             //chamado de nó * *, remetendo à classe do .NET Framework denominada 
@@ -170,20 +170,20 @@ namespace Program02._01
             //saber se duas coleções possuem um ou mais elementos em comum, 
             //utilizaremos `HashSet<T>`.
 
-            var pares = new List<int> { 0, 2, 4, 6, 8, 10 };
-            var impares = new List<int> { 1, 3, 5, 7, 9 };
-            var primos = new List<int> { 1, 3, 5, 7, 9 };
+            //var pares = new List<int> { 0, 2, 4, 6, 8, 10 };
+            //var impares = new List<int> { 1, 3, 5, 7, 9 };
+            //var primos = new List<int> { 1, 3, 5, 7, 9 };
 
-            HashSet<int> zeroAdez = new HashSet<int>();
-            pares.ForEach(n => zeroAdez.Add(n));
-            impares.ForEach(n => zeroAdez.Add(n));
-            primos.ForEach(n => zeroAdez.Add(n));
+            //TIPO_COLECAO<int> zeroAdez = new TIPO_COLECAO<int>();
+            //pares.ForEach(n => zeroAdez.INCLUIR(n));
+            //impares.ForEach(n => zeroAdez.INCLUIR(n));
+            //primos.ForEach(n => zeroAdez.INCLUIR(n));
 
-            foreach (var n in zeroAdez)
-            {
-                Console.WriteLine(n);
-            }
-            Console.WriteLine();
+            //foreach (var n in zeroAdez)
+            //{
+            //    Console.WriteLine(n);
+            //}
+            //Console.WriteLine();
 
             //Outro tipo de coleção ideal para a busca de um valor a partir de 
             //uma chave a ser armazenada(um cliente por um CPF, por exemplo, ou 
@@ -191,21 +191,21 @@ namespace Program02._01
             //`Dictionary<TKey,TValue>`, para o qual forneceremos o tipo 
             //da chave e do valor.
 
-            Dictionary<string, string> weekDays = new Dictionary<string, string>
-            {
-                { "SEG", "Monday" },
-                { "TER", "Tuesday" },
-                { "QUA", "Wednesday" },
-                { "QUI", "Thursday" },
-                { "SEX", "Friday" }
-            };
-            weekDays.Add("SAB", "Saturday");
-            weekDays.Add("DOM", "Sunday");
+            //TIPO_COLECAO<string, string> weekDays = new TIPO_COLECAO<string, string>
+            //{
+            //    { "SEG", "Monday" },
+            //    { "TER", "Tuesday" },
+            //    { "QUA", "Wednesday" },
+            //    { "QUI", "Thursday" },
+            //    { "SEX", "Friday" }
+            //};
+            //weekDays.Add("SAB", "Saturday");
+            //weekDays.Add("DOM", "Sunday");
 
-            foreach (var chaveValor in weekDays)
-            {
-                Console.WriteLine("{0} - {1}", chaveValor.Key, chaveValor.Value);
-            }
+            //foreach (var chaveValor in weekDays)
+            //{
+            //    Console.WriteLine("{0} - {1}", chaveValor.Key, chaveValor.Value);
+            //}
 
             Console.ReadLine();
         }
