f902dbde80244fc5db6589169470c180c9d4dcb8
marcelooliveira <mclricardo@gmail.com>
2019-02-28 18:14:25 -0300

    .

--- certificacao-csharp-pt12/depois/Program06.02/Program.cs
+++ certificacao-csharp-pt12/depois/Program06.02/Program.cs
@@ -8,84 +8,58 @@ namespace Program06._02
     {
         static void Main(string[] args)
         {
+            //TAREFA : CRIPTOGRAFAR A MENSAGEM ABAIXO
+            //USANDO O PADRÃO AES (ADVANCED ENCRYPTION STANDARD)
+
             string mensagemSecreta = "Dados secretos que precisam ser protegidos";
 
-            // array de bytes para manter a mensagem criptografada
-            byte[] textoCifrado;
+            // 1. array de bytes para manter a mensagem criptografada
+            byte[] textoCifrado = new byte[0];
+            // 2. matriz de bytes para manter a chave usada para criptografia
+            byte[] chave = new byte[0];
 
-            // matriz de bytes para manter a chave usada para criptografia
-            byte[] chave;
-
-            // matriz de bytes para manter o vetor de inicialização que foi usado para criptografia
-            byte[] vetorInicializacao;
-
-            // Cria uma instância de Aes
+            // 3. Cria uma instância de Aes
             // Isso cria uma chave aleatória e um vetor de inicialização
             using (Aes aes = Aes.Create())
             {
-                // copia a chave e o vetor de inicialização
+                // 3.1. copia a chave
                 chave = aes.Key;
-                vetorInicializacao = aes.IV;
 
-                // cria um criptografador para criptografar alguns dados
-                ICryptoTransform encriptador = aes.CreateEncryptor();
+                // 3.2 cria um criptografador para criptografar alguns dados
+                ICryptoTransform cryptoTransform = aes.CreateEncryptor();
 
-                // Cria um novo fluxo de memória para receber os
+                // 3.3 Cria um novo fluxo de memória para receber os
                 // dados criptografados.
-                using (MemoryStream fluxoMemoria = new MemoryStream())
+                using (MemoryStream memoryStream = new MemoryStream())
                 {
-                    // crie um CryptoStream, diga ao stream para gravar
+                    // 3.4. crie um CryptoStream, diga ao stream para gravar
                     // e o encriptador para usar. Também defina o modo
-                    using (CryptoStream fluxoEncriptado = new CryptoStream(fluxoMemoria,
-                        encriptador, CryptoStreamMode.Write))
+                    using (CryptoStream cryptoStream =
+                        new CryptoStream(memoryStream, cryptoTransform,
+                         CryptoStreamMode.Write))
                     {
-                        // cria um gravador de fluxo a partir do cryptostream
-                        using (StreamWriter escritor = new StreamWriter(fluxoEncriptado))
+                        // 3.5 cria um gravador de fluxo a partir do cryptostream
+                        using (StreamWriter streamWriter =
+                            new StreamWriter(cryptoStream))
                         {
-                            // Escreva a mensagem secreta para o fluxo.
-                            escritor.Write(mensagemSecreta);
+                            // 3.6 Escreva a mensagem secreta para o fluxo.
+                            streamWriter.Write(mensagemSecreta);
                         }
-                        // obtém a mensagem criptografada do fluxo
-                        textoCifrado = fluxoMemoria.ToArray();
+
+                        // 3.7 obtém a mensagem criptografada do fluxo
+                        textoCifrado = memoryStream.ToArray();
                     }
                 }
             }
 
-            // Exibe nossos dados
-            Console.WriteLine("Texto a ser encriptado: {0}", mensagemSecreta);
+            // 4. Exibir o texto, a chave e o texto encriptado
+            Console.WriteLine("Mensagem original: {0}", mensagemSecreta);
             ExibirBytes("Chave: ", chave);
-            ExibirBytes("Vetor de inicialização: ", vetorInicializacao);
-            ExibirBytes("Encriptada: ", textoCifrado);
+            ExibirBytes("Texto encriptado: ", textoCifrado);
 
-            // Agora faça a descriptografia
-            string textoDecifrado;
-
-            using (Aes aes = Aes.Create())
-            {
-                // Configure as instâncias aes com a chave e
-                // vetor de inicialização a ser usado para a descriptografia
-                aes.Key = chave;
-                aes.IV = vetorInicializacao;
-
-                // Cria um decodificador de aes
-                ICryptoTransform decodificador = aes.CreateDecryptor();
-
-                using (MemoryStream fluxoMemoria = new MemoryStream(textoCifrado))
-                {
-                    using (CryptoStream fluxoCripto =
-                        new CryptoStream(fluxoMemoria, decodificador, CryptoStreamMode.Read))
-                    {
-                        using (StreamReader srDecrypt = new StreamReader(fluxoCripto))
-                        {
-                            // Leia os bytes descriptografados do fluxo de decriptografia
-                            // e coloque-os em uma string.
-                            textoDecifrado = srDecrypt.ReadToEnd();
-                        }
-                    }
-                }
-            }
-
-            Console.WriteLine("Texto decifrado: {0}", textoDecifrado);
+            //TAREFA : DESCRIPTOGRAFAR OS DADOS DA VARIÁVEL textoCifrado
+            //USANDO O PADRÃO AES (ADVANCED ENCRYPTION STANDARD)
+            //E USANDO VETOR DE INICIALIZAÇÃO
 
             Console.ReadLine();
         }
