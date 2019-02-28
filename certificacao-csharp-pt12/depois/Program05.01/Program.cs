8a30b2b4b233170ec8f2293e56d5c689a582df4b
marcelooliveira <mclricardo@gmail.com>
2019-02-28 15:29:33 -0300

    .

--- certificacao-csharp-pt12/depois/Program05.01/Program.cs
+++ certificacao-csharp-pt12/depois/Program05.01/Program.cs
@@ -6,36 +6,10 @@ namespace Program05._01
     {
         static void Main(string[] args)
         {
-            const string numero = "123";
-            int resultado;
+            //TAREFA: EXIBIR O DOBRO DE UM NÚMERO DIGITADO, 
+            //VALIDANDO CONTRA STRING VAZIA, ESPAÇOS EM BRANCO E NÚMERO INVÁLIDO
 
-            if (int.TryParse(numero, out resultado))
-                Console.WriteLine("Este é um número VÁLIDO");
-            else
-                Console.WriteLine("Este é um número INVÁLIDO");
-
-            Console.WriteLine(resultado);
-
-            Console.ReadKey();
-        }
-
-        static void Main2(string[] args)
-        {
-            string stringValor = "99";
-
-            int intValor = Convert.ToInt32(stringValor);
-            Console.WriteLine("intValor: {0}", intValor);
-
-
-            // intValue = int.Parse(null);
-            bool b = int.TryParse(null, out intValor);
-            intValor = Convert.ToInt32("boom");
-
-            stringValor = "True";
-            bool boolValor = Convert.ToBoolean(null);
-            Console.WriteLine("boolValor: {0}", boolValor);
-
-            Console.ReadKey();
+            Console.ReadLine();
 
         }
     }
